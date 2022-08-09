59d59fcebca5c47bf6b87be5a124d70db85a5a46
Benton-Michael <mbenton002@gmail.com>
2022-08-09 17:04:35 -0600

    Built folder structure and files for the front-end, installed front-end dependencies (framer-motion, sanity, node-sass, react-icons, and ready to add logic for Navbar

---
+++ client_react/src/container/index.js
@@ -0,0 +1,13 @@
+export { default as About } from './About/About';
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Skills } from './Skills/Skills';
+export { default as Testimonial } from './Testimonial/Testimonial';
+export { default as Work } from './Work/Work';
+
+/* 
+
+This file allows us to go back to App.js and
+import components in one single line (instead of as a default)
+
+*/
